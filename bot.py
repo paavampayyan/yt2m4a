11f4ca7874aff0b4b4355d00fcf3f2c90ddf31da
#ItsMe <53238397+ballicipluck@users.noreply.github.com>
2021-04-15 22:39:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,21 +44,24 @@ def a(client, message):
             count += 1
         # results = YoutubeSearch(query, max_results=1).to_dict()
         try:
-            link = f"https://youtube.com{results[0]['url_suffix']}"
-            # print(results)
-            title = results[0]["title"]
-            thumbnail = results[0]["thumbnails"][0]
             duration = results[0]["duration"]
-
             ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
             # if time_to_seconds(duration) >= 1800:  # duration limit
             #     m.edit("Exceeded 30mins cap")
             #     return
-
+            
+            
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
             views = results[0]["views"]
-            thumb_name = f'thumb{message.message_id}.jpg'
-            thumb = requests.get(thumbnail, allow_redirects=True)
-            open(thumb_name, 'wb').write(thumb.content)
+            
+#             thumb_name = f'thumb{message.message_id}.jpg'
+#             thumb = requests.get(thumbnail, allow_redirects=True)
+#             open(thumb_name, 'wb').write(thumb.content)
+            
+            
 
         except Exception as e:
             print(e)
@@ -81,7 +84,7 @@ def a(client, message):
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
+        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumbnail)
         m.delete()
     except Exception as e:
         m.edit('❌ Error')
